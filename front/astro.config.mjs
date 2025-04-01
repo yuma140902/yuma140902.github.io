cb2d02e5372bd0266b24169a2f1a47456427bb60
yuma14 <yuma140902@gmail.com>
2025-04-01 19:40:01 +0900

    ci: fix

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -14,6 +14,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yuma14.net',
+  outDir: '../dist',
   integrations: [icon(), sitemap(), react(), mdx()],
   redirects: {
     '/deploys/': '/monitor/',
