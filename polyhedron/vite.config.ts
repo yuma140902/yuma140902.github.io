50c785a4868349dea0b8df50952d69afd85971ce
yuma14 <yuma140902@gmail.com>
2025-05-12 01:47:06 +0900

    build: change vite library build config

--- polyhedron/vite.config.ts
+++ polyhedron/vite.config.ts
@@ -1,13 +1,29 @@
+import path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { name as rawName } from './package.json';
+
+const name = rawName.replace(/\//g, '-').replace(/@/g, '');
 
 export default defineConfig({
   plugins: [react()],
   build: {
+    /* Reference:
+    https://github.com/IgnacioNMiranda/vite-component-library-template */
     lib: {
-      entry: './src/index.ts',
-      name: 'index',
-      fileName: 'index',
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name,
+      formats: ['es', 'umd'],
+      fileName: (format) => `${name}.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
   },
 });
