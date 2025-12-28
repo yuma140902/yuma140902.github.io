afbb90904c82231dbe261f465d52c3d8e2164d3b
yuma14 <yuma140902@gmail.com>
2025-12-29 02:53:20 +0900

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,31 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yuma14.net',
   markdown: {
     rehypePlugins: [
       'rehype-slug',
     ],
   },
 
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap({
+    customPages: [
+      'https://yuma14.net/web-image-editor/',
+      'https://yuma14.net/regend-webui/',
+      'https://yuma14.net/dotfiles-public/',
+      'https://yuma14.net/tempura-doc/',
+      'https://yuma14.net/tempura-example/sample.html',
+      'https://yuma14.net/sanmoku-next-js/',
+      'https://yuma14.net/gomoku-react/',
+      'https://yuma14.net/gomoku-blazor/',
+      'https://yuma14.net/gomoku-elm/',
+      'https://yuma14.net/simple-canvas-game/',
+      'https://yuma14.net/typing/',
+      'https://yuma14.net/Reverie/',
+    ]
+  })],
 });
