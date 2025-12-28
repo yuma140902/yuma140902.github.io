843ad61208079a79cf0d662ba62cfb5bc395389b
yuma14 <yuma140902@gmail.com>
2025-12-29 02:29:21 +0900

    use mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
@@ -9,4 +11,6 @@ export default defineConfig({
       ['rehype-toc', { headings: ['h2', 'h3'] }],
     ],
   },
+
+  integrations: [mdx()],
 });
