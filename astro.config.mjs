888e3ec256b54c72825d7bc1d0767eb7940e036f
yuma14 <yuma140902@gmail.com>
2026-03-14 20:18:29 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,27 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   site: 'https://yuma14.net',
   outDir: './dist',
-  integrations: [icon(), sitemap(), react(), mdx()],
+  integrations: [
+    icon(),
+    sitemap({
+      customPages: [
+        'https://yuma14.net/web-image-editor/',
+        'https://yuma14.net/regend-webui/',
+        'https://yuma14.net/dotfiles-public/',
+        'https://yuma14.net/tempura-doc/',
+        'https://yuma14.net/tempura-example/sample.html',
+        'https://yuma14.net/sanmoku-next-js/',
+        'https://yuma14.net/gomoku-react/',
+        'https://yuma14.net/gomoku-blazor/',
+        'https://yuma14.net/gomoku-elm/',
+        'https://yuma14.net/simple-canvas-game/',
+        'https://yuma14.net/typing/',
+        'https://yuma14.net/Reverie/',
+      ]
+    }),
+    react(),
+    mdx()
+  ],
   redirects: {
     '/deploys/': '/monitor/',
     '/services/': '/monitor/',
