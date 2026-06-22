506d1d731fd23e42371d6d94b8fc07bdf2362e36
yuma14 <yuma14@sslab.ics.keio.ac.jp>
2026-06-23 04:54:16 +0900

    fix

--- src/pages/ogp.png.ts
+++ src/pages/ogp.png.ts
@@ -2,7 +2,5 @@ import type { APIRoute } from 'astro';
 import { responseOgImage } from '../ogp/generate_ogp';
 
 export const GET: APIRoute = async () => {
-  return responseOgImage(
-    'yuma14.net - Rust、Neovim、ゲーム開発などに関する情報とツール',
-  );
+  return responseOgImage('yuma14.net - Rust、Neovim、ゲーム開発など');
 };
